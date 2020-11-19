ceead8c1baa667ba94f483759a43d26094376346
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-19 16:22:57 +0530

    Update sketch.js
    Adjust the position of the dustbin image to cover the box properly

--- sketch.js
+++ sketch.js
@@ -4,9 +4,10 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var ball,ground,box1,box2,box3;
+var dustbinImage;
 function preload()
 {
-	
+ dustbinImage = loadImage("dustbingreen.png");	
 }
 
 function setup() {
@@ -23,21 +24,21 @@ function setup() {
 	box2 = new Dustbin(1450,650,20,100);
 	box3 = new Dustbin(1390,650,20,100);
 
-	keyPressed ();
-
+	
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-rectMode(CENTER);
+
 background(0);
 box1.display();
 box2.display();
 box3.display();
 ground.display();
 ball.display();
+	image(dustbinImage,1390,600,100,100);
  
 }
 function keyPressed (){
